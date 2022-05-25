454409d43dfbad4afcc7654b47db5b3003cef253
BanerjeeProdipta <prodiptaj@live.com>
2022-05-25 22:20:50 +0600

    feat: homefeed slider

--- src/components/ui/landing/HomeFeed.tsx
+++ src/components/ui/landing/HomeFeed.tsx
@@ -21,62 +21,55 @@ function HomeFeed() {
         {...settings} className="w-full text-left">
         {campaignData.map((campaign) => (
           <div key={campaign.id} className="w-full px-2 py-2 mb-4">
-            <div className="flex">
+            <div className='flex items-center justify-between'>
               <Link
+                className='flex items-center flex-shrink-0 w-10 h-10 space-x-2'
                 to={`/user/${campaign.creatorData.id}`}
               >
                 <img
-                  className="flex flex-shrink-0 rounded-full w-14 h-14"
+                  className="flex flex-shrink-0 w-10 h-10 rounded-full"
                   src={campaign.creatorData.profilePicture}
                   alt=""
                 />
+                <p className='whitespace-nowrap'>  {campaign.creatorData.name}</p>
               </Link>
-              <div className="w-full ml-2 rounded-xl">
-                <div className="text-sm">
-                  <Link
-                    to={`/user/${campaign.creatorData.id}`}
-                    className="mr-2 font-bold text-gray-800 hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-300"
-                  >
-                    {campaign.creatorData.name}
-                  </Link>
-
-                  <span className="mr-1 text-gray-600">
-                    {
-                      campaign.createdAt
-                    }
-                  </span>
-                </div>
-                <Link to={`/fund-request/${campaign.id}`} className="px-4 py-2 mt-2 rounded-xl">
-                  <div className="space-y-2">
-                    <p className='truncate'>
-                      {campaign.description}
-                    </p>
-                    <img
-                      src={campaign.banner}
-                      alt=""
-                      className="max-w-xl rounded-lg lg:w-full"
-                    />
-                  </div>
-                  {
-                    campaign.comments.map((comment) => (
-                      <div key={comment.id} className="pt-4 space-y-4">
-                        <div className="flex items-center rounded-full">
-                          <div className="flex flex-shrink-0">
-                            <img
-                              className="w-8 h-8 mr-2 rounded-full"
-                              src={comment.createdBy.profilePicture}
-                              alt=""
-                            />
-                          </div>
-                          <p className="text-sm opacity-80">{comment.comment}</p>
-                        </div>
-                      </div>
-                    ))
-                  }
-                </Link>
-              </div>
-
+              <span className="mr-1 text-gray-600">
+                {
+                  campaign.createdAt
+                }
+              </span>
             </div>
+            <div className="w-full ml-2 rounded-xl">
+              <Link to={`/fund-request/${campaign.id}`} className="px-4 py-2 mt-2 rounded-xl">
+                <div className="space-y-2">
+                  <p className='truncate'>
+                    {campaign.description}
+                  </p>
+                  <img
+                    src={campaign.banner}
+                    alt=""
+                    className="max-w-xl rounded-lg lg:w-full"
+                  />
+                </div>
+                {
+                  campaign.comments.map((comment) => (
+                    <div key={comment.id} className="pt-4 space-y-4">
+                      <div className="flex items-center rounded-full">
+                        <div className="flex flex-shrink-0">
+                          <img
+                            className="w-8 h-8 mr-2 rounded-full"
+                            src={comment.createdBy.profilePicture}
+                            alt=""
+                          />
+                        </div>
+                        <p className="text-sm opacity-80">{comment.comment}</p>
+                      </div>
+                    </div>
+                  ))
+                }
+              </Link>
+            </div>
+
           </div>
         ))
         }</Slider>
